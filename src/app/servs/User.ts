6128a46f655d63aad07cf75d35c3a2616fca8bac
Nadhem Ben Ali <nadhembenali@outlook.fr>
2018-12-17 13:51:36 +0100

    View

---
+++ src/app/servs/User.ts
@@ -0,0 +1,7 @@
+
+export class User {
+    constructor(private userName: String, private userPwd: String) {
+        this.userName = userName;
+        this.userPwd = userPwd;
+    }
+}
---
+++ src/app/servs/user-service.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { User } from './User';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserServiceService {
+
+  constructor() { }
+  users: User[];
+  http: HttpClient;
+
+  Getusers() {
+    this.http.
+  }
+  
+}
